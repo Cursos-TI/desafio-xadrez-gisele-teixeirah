32b8cf3ba624c1461b9d76680b8986fe5c942aa0
Gisele Teixeira <gisele.teixeirah06@gmail.com>
2025-06-20 20:02:34 -0300

    Nivel novato finalizado

--- xadrez.c
+++ xadrez.c
@@ -4,29 +4,51 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+int main()
+{
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int torre, bispo = 1, rainha = 1;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Mover a peça torre em cinco casas para a direita
+    // estrutura de repetição FOR
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("=== Desafio de Xadrez===\n\n");
+    printf("Movimentação de peças:\n\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Posição da Torre: \n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    for (torre = 1; torre <= 5; torre++)
+    {
+        printf("Direita\n");
+    }
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("\n");
+
+    // Mover a peça bispo em cinco casas para cima e à direita( Ex: cima,direita)
+    // estrutura de repetição WHILE
+
+    printf("Posição do bispo: ");
+
+    while (bispo <= 5)
+    {
+        printf("\nCima, direita\n");
+        bispo++;
+    }
+
+    printf("\n");
+
+    // Mover a peça rainha em oito casas para a esquerda
+    // estrutura de repetição DO-WHILE
+
+    printf("Posição da rainha: \n");
+
+    do
+    {
+        printf("Esquerda\n");
+        rainha++;
+    } while (rainha <= 8);
+
+    printf("\n");
 
     return 0;
 }
